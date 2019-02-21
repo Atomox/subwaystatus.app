ba8ceeffa6c76f178fb65151b00fe24ded6353ed
Ben Helmer <Atomox@gmail.com>
2019-02-21 09:41:58 -0500

    author: bhelmer -- NO-TICKET -- Tab cleanup.

--- src/shared/js/components/RouteChange.js
+++ src/shared/js/components/RouteChange.js
@@ -12,229 +12,229 @@ import rcStyle from '../../styles/RouteChange.styles';
 
 export default class RouteChange extends Component {
 
-  componentDidCatch(err, info) {
-    console.log('<!> Route Change Error', err);
-  }
+	componentDidCatch(err, info) {
+		console.log('<!> Route Change Error', err);
+	}
 
-  prepRouteObject(r) {
-    const along = this.isRouteLineChange(r);
-    return {
-      line_change: this.isRouteLineChange(r),
-      lcl: (!along && _.get(r, 'exp_lcl') == 'local') ? true : false,
-      exp: (!along && _.get(r, 'exp_lcl') == 'express') ? true : false,
-      bypass: (r.bypass && r.bypass.length > 0) ? true : false,
-      no_svc_between: (r.noTrains) ? true : false
-    };
-  }
+	prepRouteObject(r) {
+		const along = this.isRouteLineChange(r);
+		return {
+			line_change: this.isRouteLineChange(r),
+			lcl: (!along && _.get(r, 'exp_lcl') == 'local') ? true : false,
+			exp: (!along && _.get(r, 'exp_lcl') == 'express') ? true : false,
+			bypass: (r.bypass && r.bypass.length > 0) ? true : false,
+			no_svc_between: (r.noTrains) ? true : false
+		};
+	}
 
-  isRouteLineChange(route) {
-    if (route.along && Array.isArray(route.lines)) {
-      if (route.lines.indexOf(route.along) === -1) {
-        return true;
-      }
-    }
-    return false;
-  }
+	isRouteLineChange(route) {
+		if (route.along && Array.isArray(route.lines)) {
+			if (route.lines.indexOf(route.along) === -1) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-  getRouteAlong(route, rObj) {
-    return (rObj.line_change)
-      ? (<TrainLine
-          key={_.uniqueId('train-' + mta.getlineById(route.along))}
-          line={mta.getlineById(route.along)}
-          dir={'both'}
-          styleType='small'/>)
-      : (<Txt> run between</Txt>)
-  }
+	getRouteAlong(route, rObj) {
+		return (rObj.line_change)
+			? (<TrainLine
+					key={_.uniqueId('train-' + mta.getlineById(route.along))}
+					line={mta.getlineById(route.along)}
+					dir={'both'}
+					styleType='small'/>)
+			: (<Txt> run between</Txt>)
+	}
 
-  getRouteFrom(route) {
-    return (route.from)
-      ? (
-          <Station
-            key={_.uniqueId('station-' + route.from)}
-            stations={this.props.stations}
-            line={_.union([route.along],route.lines)}
-            sid={route.from}/>
-        )
-      : null;
-  }
+	getRouteFrom(route) {
+		return (route.from)
+			? (
+					<Station
+						key={_.uniqueId('station-' + route.from)}
+						stations={this.props.stations}
+						line={_.union([route.along],route.lines)}
+						sid={route.from}/>
+				)
+			: null;
+	}
 
-  getRouteTo(route) {
-    return (route.to)
-      ? (
-          <Station
-            key={_.uniqueId('station-' + route.to)}
-            stations={this.props.stations}
-            line={_.union([route.along],route.lines)}
-            sid={route.to}/>
-        )
-      : null;
-  }
+	getRouteTo(route) {
+		return (route.to)
+			? (
+					<Station
+						key={_.uniqueId('station-' + route.to)}
+						stations={this.props.stations}
+						line={_.union([route.along],route.lines)}
+						sid={route.to}/>
+				)
+			: null;
+	}
 
-  getRouteBoroGeneral(route) {
-    return (route.in)
-      ? (<Txt> { route.in }</Txt>)
-      : null;
-  }
+	getRouteBoroGeneral(route) {
+		return (route.in)
+			? (<Txt> { route.in }</Txt>)
+			: null;
+	}
 
-  getRouteTrains(route) {
-    return route.lines.map(t => (
-      <TrainLine
-        key={_.uniqueId('train-' + mta.getlineById(t))}
-        line={mta.getlineById(t)}
-        dir='both'
-        styleType='small'/>
-      ));
-  }
+	getRouteTrains(route) {
+		return route.lines.map(t => (
+			<TrainLine
+				key={_.uniqueId('train-' + mta.getlineById(t))}
+				line={mta.getlineById(t)}
+				dir='both'
+				styleType='small'/>
+			));
+	}
 
-  getRouteBypassStations(route) {
-    return (route.bypass)
-      ? (route.bypass
-          .map( s => (<Station
-            key={_.uniqueId('station-' + s)}
-            stations={this.props.stations}
-            line={_.union([route.along],route.lines)}
-            sid={s}/> ) )
-          .reduce((prev, curr) => [prev, '<Txt>, </Txt>', curr])
-        )
-      : null;
-  }
+	getRouteBypassStations(route) {
+		return (route.bypass)
+			? (route.bypass
+					.map( s => (<Station
+						key={_.uniqueId('station-' + s)}
+						stations={this.props.stations}
+						line={_.union([route.along],route.lines)}
+						sid={s}/> ) )
+					.reduce((prev, curr) => [prev, '<Txt>, </Txt>', curr])
+				)
+			: null;
+	}
 
-  getRouteAction(route, rObj) {
-    if (route.action === 'replace') {
-      return 'replace the';
-    }
-    else if (rObj.line_change) {
-      return 'via the';
-    }
-    else if (rObj.no_svc_between) {
-      return 'service';
-    }
-    else if (rObj.bypass) {
-      return 'skip';
-    }
+	getRouteAction(route, rObj) {
+		if (route.action === 'replace') {
+			return 'replace the';
+		}
+		else if (rObj.line_change) {
+			return 'via the';
+		}
+		else if (rObj.no_svc_between) {
+			return 'service';
+		}
+		else if (rObj.bypass) {
+			return 'skip';
+		}
 //        else if (r.section) {   action = 'section ' + r.section; }
-    else if (rObj.lcl || rObj.exp) {
-      return 'run ' + route.exp_lcl;
-    }
+		else if (rObj.lcl || rObj.exp) {
+			return 'run ' + route.exp_lcl;
+		}
 
-    return 'run';
-  }
+		return 'run';
+	}
 
-  getRoutePrefix(route, rObj) {
-    let pre = (route.section)
-      ? '(' + route.section + ')'
-      : '';
+	getRoutePrefix(route, rObj) {
+		let pre = (route.section)
+			? '(' + route.section + ')'
+			: '';
 
-    if (route.allTrains === false) {
-      pre += (pre.length > 0)
-        ? ' ' + 'Some'
-        : 'Some';
-    }
-    if (rObj.no_svc_between) {
-      pre += (pre.length > 0)
-        ? ' ' + 'No'
-        : 'No';
-    }
+		if (route.allTrains === false) {
+			pre += (pre.length > 0)
+				? ' ' + 'Some'
+				: 'Some';
+		}
+		if (rObj.no_svc_between) {
+			pre += (pre.length > 0)
+				? ' ' + 'No'
+				: 'No';
+		}
 
-    return pre;
-  }
+		return pre;
+	}
 
-  getRoutes() {
-    return this.props.routeInfo.route.map(r => {
-      try {
+	getRoutes() {
+		return this.props.routeInfo.route.map(r => {
+			try {
 
-        // Along null is running on same line between stations.
-        if (r.along == null) {
-          r.along = r.lines[0];
-        }
+				// Along null is running on same line between stations.
+				if (r.along == null) {
+					r.along = r.lines[0];
+				}
 
-        const rObj = this.prepRouteObject(r);
+				const rObj = this.prepRouteObject(r);
 
-        let trains = this.getRouteTrains(r);
-        let along = this.getRouteAlong(r, rObj);
-        let from = this.getRouteFrom(r);
-        let to = this.getRouteTo(r);
-        let boro_general = this.getRouteBoroGeneral(r);
-        let bypass_stations = this.getRouteBypassStations(r);
-        let action = this.getRouteAction(r, rObj);
-        let pre = this.getRoutePrefix(r, rObj);
+				let trains = this.getRouteTrains(r);
+				let along = this.getRouteAlong(r, rObj);
+				let from = this.getRouteFrom(r);
+				let to = this.getRouteTo(r);
+				let boro_general = this.getRouteBoroGeneral(r);
+				let bypass_stations = this.getRouteBypassStations(r);
+				let action = this.getRouteAction(r, rObj);
+				let pre = this.getRoutePrefix(r, rObj);
 
 
-        function getBypassText() {
-          return (<Txt styles={[rcStyle.flex1,rcStyle.text]}>{ bypass_stations }.</Txt>);
-        }
+				function getBypassText() {
+					return (<Txt styles={[rcStyle.flex1,rcStyle.text]}>{ bypass_stations }.</Txt>);
+				}
 
-        function getBoroGeneralText(boro_general) {
-          return (<Txt styles={[rcStyle.flex1,rcStyle.text]}>in { boro_general }.</Txt>);
-        }
+				function getBoroGeneralText(boro_general) {
+					return (<Txt styles={[rcStyle.flex1,rcStyle.text]}>in { boro_general }.</Txt>);
+				}
 
-        function getNormalText(from, to) {
-          return (<Txt styles={[rcStyle.flex1,rcStyle.text]}>from {from} until {to}.</Txt>);
-        }
+				function getNormalText(from, to) {
+					return (<Txt styles={[rcStyle.flex1,rcStyle.text]}>from {from} until {to}.</Txt>);
+				}
 
-        function getBetweenText(from, to) {
-          return (<Txt styles={[rcStyle.flex1,rcStyle.text]}>between {from} and {to}.</Txt>);
-        }
+				function getBetweenText(from, to) {
+					return (<Txt styles={[rcStyle.flex1,rcStyle.text]}>between {from} and {to}.</Txt>);
+				}
 
-  			return (
-          <View key={_.uniqueId()} style={ rcStyle.container }>
+				return (
+					<View key={_.uniqueId()} style={ rcStyle.container }>
 
-            <Txt styles={ rcStyle.pre }>{ pre }</Txt>
+						<Txt styles={ rcStyle.pre }>{ pre }</Txt>
 
-            { trains }
+						{ trains }
 
-            <Txt styles={ rcStyle.lineMessageContainer }>
+						<Txt styles={ rcStyle.lineMessageContainer }>
 
-              { /**
+							{ /**
 
-                  @TODO -- We need to unwrap this block from txt, while maintaining a line wrap.
+									@TODO -- We need to unwrap this block from txt, while maintaining a line wrap.
 
-                      iOS looks like be breaking the - along - out of the txt, which causes a margin of 2, and screwes up spacing of the message. The Line is bumping right a split second after the load, and overlapping with the following word.
+											iOS looks like be breaking the - along - out of the txt, which causes a margin of 2, and screwes up spacing of the message. The Line is bumping right a split second after the load, and overlapping with the following word.
 
-                 */ }
-                <Txt styles={ [rcStyle.text, rcStyle.lineSegment] }> { action } </Txt>
+								 */ }
+								<Txt styles={ [rcStyle.text, rcStyle.lineSegment] }> { action } </Txt>
 
-                { rObj.line_change && along }
+								{ rObj.line_change && along }
 
-                { // No stations, just "in Boro".
-                  (boro_general) &&
-                  getBoroGeneralText(boro_general) }
+								{ // No stations, just "in Boro".
+									(boro_general) &&
+									getBoroGeneralText(boro_general) }
 
-                { // No stations, just "in Boro".
-                  (!boro_general && bypass_stations) &&
-                  getBypassText(bypass_stations) }
+								{ // No stations, just "in Boro".
+									(!boro_general && bypass_stations) &&
+									getBypassText(bypass_stations) }
 
-                { // Normal Stations from/to.
-                  (!boro_general && !bypass_stations) && !rObj.no_svc_between &&
-                  getNormalText(from, to) }
+								{ // Normal Stations from/to.
+									(!boro_general && !bypass_stations) && !rObj.no_svc_between &&
+									getNormalText(from, to) }
 
-                { // Normal Stations from/to.
-                  (rObj.no_svc_between) &&
-                  getBetweenText(from, to) }
+								{ // Normal Stations from/to.
+									(rObj.no_svc_between) &&
+									getBetweenText(from, to) }
 
-            </Txt>
-          </View>
-  			);
-      }
-      catch (err) {
-        console.error('Problem with route change: ', r, '|', err);
-      }
+						</Txt>
+					</View>
+				);
+			}
+			catch (err) {
+				console.error('Problem with route change: ', r, '|', err);
+			}
 
 		});
-  }
+	}
 
-  render() {
+	render() {
 		return (
 			<View>
-        { // className="route-change"
-          this.getRoutes()
-       }
+				{ // className="route-change"
+					this.getRoutes()
+			 }
 			</View>
 		);
 	}
 }
 
 RouteChange.propTypes = {
-  routeInfo: PropTypes.any.isRequired,
-  stations: PropTypes.any.isRequired,
+	routeInfo: PropTypes.any.isRequired,
+	stations: PropTypes.any.isRequired,
 };
--- test/routechange.test.js
+++ test/routechange.test.js
@@ -11,42 +11,42 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('Route Change Card', () => {
 
-  function assembleRouteInstance(route) {
-    const routeInfo = {
-      route: route
-    };
+	function assembleRouteInstance(route) {
+		const routeInfo = {
+			route: route
+		};
 
-    const wrapper = shallow(<RouteChange routeInfo={ routeInfo } stations={ {} } />);
-    const instance = wrapper.instance();
+		const wrapper = shallow(<RouteChange routeInfo={ routeInfo } stations={ {} } />);
+		const instance = wrapper.instance();
 
-    return instance;
-  }
+		return instance;
+	}
 
-  describe('Route Change Detection', () => {
+	describe('Route Change Detection', () => {
 
-    describe('Line Change (AoverC)', () => {
-      const route = [
-        {
-          allTrains: false,
-          dir: null,
-          lines: ["W"],
-          along: "D",
-          from: "Bk26-R30",
-          to: "Bk59-B12"
-        }
-      ];
+		describe('Line Change (AoverC)', () => {
+			const route = [
+				{
+					allTrains: false,
+					dir: null,
+					lines: ["W"],
+					along: "D",
+					from: "Bk26-R30",
+					to: "Bk59-B12"
+				}
+			];
 
-      const instance = assembleRouteInstance(route);
-      const result = instance.prepRouteObject(route[0]);
+			const instance = assembleRouteInstance(route);
+			const result = instance.prepRouteObject(route[0]);
 
-      it('Line Change is true', () => expect(result.line_change).toEqual(true));
-      it('Bypass is false', () => expect(result.bypass).toEqual(false));
+			it('Line Change is true', () => expect(result.line_change).toEqual(true));
+			it('Bypass is false', () => expect(result.bypass).toEqual(false));
 
-    });
+		});
 
-    describe('Running Express', () => {
-      const route = [
-        {
+		describe('Running Express', () => {
+			const route = [
+				{
 					allTrains: true,
 					dir: null,
 					exp_lcl: 'express',
@@ -57,19 +57,19 @@ describe('Route Change Card', () => {
 					process: 'RouteChangeStandard',
 					in: 'Manhattan'
 				},
-      ];
+			];
 
-      const instance = assembleRouteInstance(route);
-      const result = instance.prepRouteObject(route[0]);
+			const instance = assembleRouteInstance(route);
+			const result = instance.prepRouteObject(route[0]);
 
-      it('Line Change is false', () => expect(result.line_change).toEqual(false));
-      it('Express is true', ()=> expect(result.exp).toEqual(true));
-      it('Local is false',() => expect(result.lcl).toEqual(false));
-    });
+			it('Line Change is false', () => expect(result.line_change).toEqual(false));
+			it('Express is true', ()=> expect(result.exp).toEqual(true));
+			it('Local is false',() => expect(result.lcl).toEqual(false));
+		});
 
-    describe('Running Local', () => {
-      const route = [
-        {
+		describe('Running Local', () => {
+			const route = [
+				{
 					allTrains: true,
 					dir: "[mn151-a12]-bound",
 					exp_lcl: "local",
@@ -81,65 +81,65 @@ describe('Route Change Card', () => {
 					process: "RouteChangeStandard",
 					parsed: "[Mn151-A12]-bound [B] trainsrun local from [Bk54-D39] to [Bk42-D26]"
 				}
-      ];
+			];
 
-      const instance = assembleRouteInstance(route);
-      const result = instance.prepRouteObject(route[0]);
+			const instance = assembleRouteInstance(route);
+			const result = instance.prepRouteObject(route[0]);
 
-      it('Line Change is false', () => expect(result.line_change).toEqual(false));
-      it('Express is false', () => expect(result.exp).toEqual(false));
-      it('Local is true', () => expect(result.lcl).toEqual(true));
-    });
+			it('Line Change is false', () => expect(result.line_change).toEqual(false));
+			it('Express is false', () => expect(result.exp).toEqual(false));
+			it('Local is true', () => expect(result.lcl).toEqual(true));
+		});
 
 
-    describe('Bypass Stations', () => {
-      const route = [
-        {
-          allTrains: true,
-          lines: ["4"],
-          along: null,
-          bypass: [
-            "Bx385-410",
-            "Bx386-411",
-            "Bx387-412",
-            "Bx388-413",
-            "Bx604-414"
-          ],
-          action: "bypass",
-          section: null,
-        }
-      ];
+		describe('Bypass Stations', () => {
+			const route = [
+				{
+					allTrains: true,
+					lines: ["4"],
+					along: null,
+					bypass: [
+						"Bx385-410",
+						"Bx386-411",
+						"Bx387-412",
+						"Bx388-413",
+						"Bx604-414"
+					],
+					action: "bypass",
+					section: null,
+				}
+			];
 
-      const instance = assembleRouteInstance(route);
-      const result = instance.prepRouteObject(route[0]);
+			const instance = assembleRouteInstance(route);
+			const result = instance.prepRouteObject(route[0]);
 
-      it('Line change is false', () => expect(result.line_change).toEqual(false));
-      it('Bypass is true', () => expect(result.bypass).toEqual(true));
-    });
+			it('Line change is false', () => expect(result.line_change).toEqual(false));
+			it('Bypass is true', () => expect(result.bypass).toEqual(true));
+		});
 
-    describe('No Service Between', () => {
-      const route = [
-        {
+		describe('No Service Between', () => {
+			const route = [
+				{
 					noTrains: true,
-			    allTrains: true,
-			    dir: null,
-			    lines: [ 'E' ],
-			    along: null,
-			    from: 'Qs616-G14',
-			    to: 'Mn167-A32',
-			    section: null,
-			    action: 'No',
-			    parsed: '[E] No service between [Qs616-G14] and [Mn167-A32]'
+					allTrains: true,
+					dir: null,
+					lines: [ 'E' ],
+					along: null,
+					from: 'Qs616-G14',
+					to: 'Mn167-A32',
+					section: null,
+					action: 'No',
+					parsed: '[E] No service between [Qs616-G14] and [Mn167-A32]'
 				},
-      ];
+			];
 
-      const instance = assembleRouteInstance(route);
-      const result = instance.prepRouteObject(route[0]);
+			const instance = assembleRouteInstance(route);
+			const result = instance.prepRouteObject(route[0]);
 
-      console.log();
+			console.log();
 
-      it('Line change is false', () => expect(result.line_change).toEqual(false));
-      it('No SVC Between is true', () => expect(result.no_svc_between).toEqual(true));
-    });
-  });
+			it('Line change is false', () => expect(result.line_change).toEqual(false));
+			it('No SVC Between is true', () => expect(result.no_svc_between).toEqual(true));
+		});
+	});
 });
